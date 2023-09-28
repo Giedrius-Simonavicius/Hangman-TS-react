ca0b13764f7a3db542f7cd6234a675d7ed37eaf2
Giedrius_Simonavicius <giedrius.simonavicius123@gmail.com>
2023-09-28 14:42:05 +0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import words from "./wordList.json";
+import { HangmanDrawing } from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
@@ -9,8 +12,20 @@ function App() {
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   return (
-    <div>
-      <h1>hi</h1>
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0, auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
     </div>
   );
 }
